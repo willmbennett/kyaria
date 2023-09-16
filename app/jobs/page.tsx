774c98a655cac5e10de527b17f5a820885b671a2
Will Bennett <41313772+willmbennett@users.noreply.github.com>
2023-09-15 18:33:20 -0700

    made it so you can't see jobs without a profile

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -6,9 +6,11 @@ import { expectedJson, defaultTextInput, demoJSON, FormFields } from './job-help
 import NewJobForm from "../components/jobs/NewJobForm";
 //import { ObjectId } from "mongodb";
 import { JobsContext } from "../components/jobs/JobsContext";
+import { useSession } from 'next-auth/react';
 
 export default function JobPage() {
-  let {userProfile, userJobs } = useContext(JobsContext)
+  const { data: session } = useSession();
+  let { userProfile, userJobs } = useContext(JobsContext)
   const [creatingJob, setCreatingJob] = useState(false);
   const [defaultValue, setDefaultValue] = useState<FormFields>(expectedJson);
 
@@ -23,51 +25,61 @@ export default function JobPage() {
     <div className="flex max-w-5xl mx-auto flex-col items-center py-2 min-h-screen bg-gray-100">
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4">
         <div className="w-full max-w-2xl bg-white rounded-xl p-6 shadow-md">
-          {!creatingJob && (
-            <>
-              <h1 className="sm:text-6xl text-4xl max-w-2xl font-bold text-slate-900 mb-10">
-                Your Jobs
-              </h1>
-              <ul className="space-y-4 mb-8">
-                {userJobs.map((job) => (
-                  <li key={job._id} className="text-lg text-slate-700">
-                    <Link href={`/jobs/${job._id}`}>
-                      <span className="cursor-pointer hover:text-slate-900 hover:underline">
-                        {job.jobTitle}
-                      </span>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-              <button className="bg-blue-500 text-white px-4 py-2 rounded mt-4" onClick={handleCreateJobClick}> New Job</button>
-            </>
-          )}
-          {creatingJob && (
-            <>
+          {userProfile.name && (<>
             <h1 className="sm:text-6xl text-4xl max-w-2xl font-bold text-slate-900 mb-10">
-          Create a New Job
-        </h1>
-        
-            <div className="mb-4 flex">
-              {defaultValue.jobTitle.length == 0 && (
-                <TextToJSON
-                  setDefaultValue={setDefaultValue}
-                  expectedJson={expectedJson}
-                  defaultTextInput={defaultTextInput}
-                  demoJSON={demoJSON}
-                  inputTextType='resume'
-                />
-              )}
-              {defaultValue.jobTitle.length > 0 && (
-                <NewJobForm
-                  defaultValue={defaultValue}
-                  setCreatingJob={setCreatingJob}
-                  userId={userProfile.userId}
-                />
-              )}
-            </div>
-            </>
-          )}
+              Create a Profile First
+            </h1>
+            <Link href={`profile/${session?.user?.id}`}>
+            <button className="bg-blue-500 text-white px-4 py-2 rounded mt-4" type="submit">Go To Profile</button>
+            </Link>
+          </>)}
+          {userProfile.name && (<>
+            {!creatingJob && (
+              <>
+                <h1 className="sm:text-6xl text-4xl max-w-2xl font-bold text-slate-900 mb-10">
+                  Your Jobs
+                </h1>
+                <ul className="space-y-4 mb-8">
+                  {userJobs.map((job) => (
+                    <li key={job._id} className="text-lg text-slate-700">
+                      <Link href={`/jobs/${job._id}`}>
+                        <span className="cursor-pointer hover:text-slate-900 hover:underline">
+                          {job.jobTitle}
+                        </span>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+                <button className="bg-blue-500 text-white px-4 py-2 rounded mt-4" onClick={handleCreateJobClick}> New Job</button>
+              </>
+            )}
+            {creatingJob && (
+              <>
+                <h1 className="sm:text-6xl text-4xl max-w-2xl font-bold text-slate-900 mb-10">
+                  Create a New Job
+                </h1>
+
+                <div className="mb-4 flex">
+                  {defaultValue.jobTitle.length == 0 && (
+                    <TextToJSON
+                      setDefaultValue={setDefaultValue}
+                      expectedJson={expectedJson}
+                      defaultTextInput={defaultTextInput}
+                      demoJSON={demoJSON}
+                      inputTextType='resume'
+                    />
+                  )}
+                  {defaultValue.jobTitle.length > 0 && (
+                    <NewJobForm
+                      defaultValue={defaultValue}
+                      setCreatingJob={setCreatingJob}
+                      userId={userProfile.userId}
+                    />
+                  )}
+                </div>
+              </>
+            )}
+          </>)}
         </div>
       </main>
     </div>
