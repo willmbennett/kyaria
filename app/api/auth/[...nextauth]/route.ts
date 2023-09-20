4ab1d6df313ee00a611875c7c6e329a562285e02
Will Bennett <41313772+willmbennett@users.noreply.github.com>
2023-09-20 10:40:30 -0700

    fixed auth route

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import { authOptions } from "../../../../lib/auth";
 import NextAuth from "next-auth";
 
-export const AuthOptions = NextAuth(authOptions);
+export const handler = NextAuth(authOptions);
 
-export {AuthOptions as GET, AuthOptions as POST}
+export {handler as GET, handler as POST}
