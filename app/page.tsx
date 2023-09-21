7797cf55550683ad80b5ca15b04075d845b39fe3
Will Bennett <41313772+willmbennett@users.noreply.github.com>
2023-09-21 15:41:04 -0700

    changed div

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@
 
 export default function Page({ params }: { params: { session: any } }) {
   return (
-    <main className='top-0 absolute w-full'>
+    <div className='top-0 absolute w-full'>
       <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
         <h1 className="sm:text-6xl text-4xl max-w-[708px] font-bold text-slate-900 p-5 text-center">
             Launch your career with AI
             {params.session}
         </h1>
       </div>
-    </main>
+    </div>
   );
 }
