7e6205e662d4369f5eb9acf1de8db3d1ef593340
Will Bennett <41313772+willmbennett@users.noreply.github.com>
2023-10-05 14:21:29 -0700

    fixed item spacing

--- app/components/profile/Profile.tsx
+++ app/components/profile/Profile.tsx
@@ -109,8 +109,8 @@ export default function Profile({
                 </>
             )}
             {!error && sessionUserId == userId && !profile && inputTextView && (
-                <div>
-                    <div className='py-4 items-center flex flex-col'>
+                <div className='py-4 flex flex-col items-center justify-center text-center'>
+                    <div className='py-4 items-center flex flex-col justify-center text-center'>
                         <h1 className="sm:text-6xl text-4xl font-bold text-slate-900 mb-8">
                             Welcome!
                         </h1>
