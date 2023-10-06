1d38d416a00da2474ab8ea72f2e76f9ee241d558
Will Bennett <41313772+willmbennett@users.noreply.github.com>
2023-10-05 20:14:34 -0700

    added editing capability

--- app/components/board/ChatWithGPT.tsx
+++ app/components/board/ChatWithGPT.tsx
@@ -4,6 +4,7 @@ import { useChat } from 'ai/react';
 import { Message } from 'ai'
 import { ChatMessage } from './ChatMessage';
 import { Button } from '../Button';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 interface Props {
   documentID: string,
@@ -17,6 +18,10 @@ interface Props {
   childIndex?: number;
   jobKeyWords?: string[]
 }
+
+type FormFields = {
+  input: string;
+};
 
 export default function ChatWithGPT({
   documentID,
@@ -33,11 +38,11 @@ export default function ChatWithGPT({
   const [finishedLoading, setFinishedLoading] = useState(false)
   const [loading, setLoading] = useState(false)
 
-  const { messages, setMessages, append, stop } = useChat({
+  const { messages, setMessages, reload, append, stop } = useChat({
     body: {
       temp: temp
     },
-    initialMessages: [{ id: '1', role: 'assistant', content: currentState }],
+    initialMessages: [...message, { id: '1', role: 'assistant', content: currentState }],
     onFinish() {
       setFinishedLoading(true);
       setLoading(false);
@@ -48,8 +53,18 @@ export default function ChatWithGPT({
   const handleClick = () => {
     setFinishedLoading(false)
     setLoading(true)
-    setMessages([])
-    append(message);
+    if (edit) {
+      setEdit(false);
+      append({
+        role: "user",
+        content: `Please help me refine this: ${watch("input")}`
+      })
+    } else if (currentState == '') {
+      setMessages([])
+      append(message);
+    } else {
+      reload()
+    }
   };
   // Reload the last call
   const handleStop = () => {
@@ -57,8 +72,38 @@ export default function ChatWithGPT({
     setLoading(false);
   };
 
+  // Reload the last call
+  const handleReset = () => {
+    setEdit(false);
+    setLoading(true)
+    setMessages([])
+    append(message);
+  };
+
+  // cancel editing
+  const handleCancel = () => {
+    setEdit(false);
+    setValues({ input: currentState })
+  };
+
+  // Save an edit
+  const handleSave = async () => {
+    setEdit(false);
+    const messageSaved = watch("input")
+
+    // Save the message to the database
+    const id = documentID;
+    //console.log(`"${setKey}":"${returnedMessage}"`)
+    const data = JSON.parse(`{"${setKey}":""}`)
+    data[setKey] = messageSaved
+    lastmessage.content = messageSaved
+    //console.log(id, data)
+    const update = await saveToDatabase(id, data, "/")
+
+  };
+
   const saveMessage = async () => {
-    const returnedMessage = messages[messages.length - 1].content
+    const returnedMessage = lastmessage.content
 
     // Save the message to the database
     const id = documentID;
@@ -82,23 +127,81 @@ export default function ChatWithGPT({
     }
   }, [finishedLoading]);
 
+  const [edit, setEdit] = useState(false)
+  // Click to edit the document
+  const handleEdit = () => {
+    setValues({ input: lastmessage.content })
+    setEdit(true);
+  };
+  const [values, setValues] = useState<FormFields>();
+  const { register, watch, formState: { errors } } = useForm<FormFields>({
+    defaultValues: { input: currentState },
+    values
+  });
+
   const lastmessage = messages[messages.length - 1]
+
+  const [active, setActive] = useState(false);
+
+  const toggleActive = () => {
+    setActive(!active);
+  };
 
   return (
     <div className="flex-col flex items-center justify-center w-full">
       <div className='flex-col w-full'>
-        <div className='flex w-full justify-center'>
-          {!loading && (
+        <div className='flex flex-row justify-center'>
+          {!loading && !edit && (
+            <Button
+              variant="solid"
+              size="md"
+              onClick={handleEdit}
+              type="button"
+              className="m-3"
+            >
+              Refine
+            </Button>
+          )}
+          {!loading && edit && (<>
+            <Button
+              variant="secondary"
+              size="md"
+              onClick={handleCancel}
+              type="button"
+              className="m-3"
+            >
+              Cancel
+            </Button>
+            <Button
+              variant="solid"
+              size="md"
+              onClick={handleSave}
+              type="button"
+              className="m-3"
+            >
+              Save
+            </Button>
+            <Button
+              variant="ghost"
+              size="md"
+              onClick={handleReset}
+              type="button"
+              className="m-3"
+            >
+              Reset
+            </Button>
+          </>)}
+          {!loading && (<>
             <Button
               variant="solid"
               size="md"
               onClick={handleClick}
               type="button"
-              className="my-3"
+              className="m-3"
             >
               Generate
             </Button>
-          )}
+          </>)}
           {loading && (
             <Button
               variant="ghost"
@@ -111,8 +214,31 @@ export default function ChatWithGPT({
             </Button>
           )}
         </div>
-        {lastmessage && lastmessage.role == 'assistant' && (
-          <ChatMessage message={lastmessage} jobKeyWords={jobKeyWords || ['']}/>
+        {edit && (<>
+          <button
+            className='inline-flex w-full justify-center gap-x-1.5 rounded-md px-3 py-2 text-sm font-semibold shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50'
+            onClick={toggleActive}
+          > See instructions
+            <svg className="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+              <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
+            </svg>
+          </button>
+          {active && (<div className="sticky top-5 bg-gray-100 p-4 rounded shadow-md w-full">
+            <p className='mt-2'>Below is the answer we generated for you.</p>
+            <p className='mt-2'>If you don't like any portion, you can delete it. </p>
+            <p className='mt-2'>You can also add additional details you want to incorporate into the answer</p>
+            <p className='mt-2'><strong>Example:</strong> If you'd like the output to highlight your communication skills, append "I excel in communication".</p>
+            <p className='mt-2'>Once you've made your changes, click "generate". We'll produce a polished revision.</p>
+            <p className='mt-2'>If you want to reset the output, click reset and we'll provide a fresh version.</p>
+            <strong>Demo:</strong>
+            <img className='mt-2' src="/editing-demo.gif" alt="editing demo" />
+          </div>
+          )}
+          <textarea {...register('input', { required: true })} rows={lastmessage.content.length / 50} className='w-full' defaultValue={lastmessage.content} />
+          {errors.input && <p className="text-red-500 mt-2">Don't leave this blank</p>}
+        </>)}
+        {lastmessage && !edit && lastmessage.role == 'assistant' && (
+          <ChatMessage message={lastmessage} jobKeyWords={jobKeyWords || ['']} />
         )}
       </div>
     </div>
