027cf18ef43a7e447f2f1ad3f77569cea4f0af49
Will Bennett <41313772+willmbennett@users.noreply.github.com>
2023-10-23 17:28:12 -0700

    pdf download cover letter

--- app/components/apps/pages/CoverLetter.tsx
+++ app/components/apps/pages/CoverLetter.tsx
@@ -3,6 +3,7 @@
 import ChatWithGPT from '../../board/ChatWithGPT';
 import { updateJobAppAction } from '../../../board/_action';
 import { removeDetailSections } from '../../../apps/[id]/app-helper';
+import { CoverLetterPDF } from '../CoverLetterPDF';
 
 export default function CoverLetter({
     jobApp,
@@ -17,22 +18,7 @@ export default function CoverLetter({
         {
             "role": "system",
             "content": `You are a professional cover letter writer specialized in creating personalized, compelling cover letters tailored to specific job descriptions. The letter should showcase the individual's experience, skills, and education, and should be organized into an introductory paragraph, a body, and a closing.
-            [Your Name]
-            [Your Address]  
-            Email: [Your Email]  
-            Phone: [Your Phone Number]  
-
-            ---
-
-            Date: September 10, 2023  
-            Google
-            [Company Address]  
-            New York City, NY  
-
-            ---
-
-            Dear Hiring Manager,
-
+            Example:
             I am writing to express my interest in the Senior Software Engineer position at Google's New York City office, as listed on your careers page. With a strong background in backend development and a passion for problem-solving, I am excited about the opportunity to contribute to Google's innovative environment.
 
             As a graduate in Computer Science with over 6 years of software development experience, I meet all the mandatory requirements for this role. I have a proven track record in developing robust software solutions, primarily using Java and Python. In my current role at [Current Company], I have designed and maintained RESTful APIs, implemented microservices architecture, and managed relational databases like MySQL. This experience, combined with my solid understanding of data structures, algorithms, and system design, makes me confident that I can contribute effectively to your team.
@@ -40,9 +26,8 @@ export default function CoverLetter({
             Beyond technical skills, I pride myself on my excellent communication abilities, which have enabled me to work effectively with cross-functional teams. I am also familiar with Google Cloud Platform and front-end technologies like React, matching your 'nice-to-have' criteria.
 
             Google's reputation for fostering an open and collaborative work environment resonates with me, and I am particularly attracted to your hybrid remote work model. I am enthusiastic about the chance to bring my unique blend of skills and experience to Google. Thank you for considering my application. I am looking forward to the opportunity to further discuss my suitability for this position in an interview.
-
-            Yours sincerely,  
-            [Your Name]
+            
+            Don't include the salutation
             `
         },
         {
@@ -63,6 +48,7 @@ export default function CoverLetter({
             <h1 className="text-center sm:text-6xl text-4xl font-bold mb-8">
                 Stand out with a cover letter
             </h1>
+
             <ChatWithGPT
                 documentID={jobApp._id}
                 setKey='userCoverLetter'
@@ -71,6 +57,18 @@ export default function CoverLetter({
                 saveToDatabase={updateJobAppAction}
                 temp={0.5}
                 jobKeyWords={jobKeyWords}
+            />
+
+            <h3 className="text-xl font-semibold mt-5">Download your cover letter as a pdf</h3>
+            <p className="text-gray-600 my-2">Once you create your cover letter you can see it here and download it.</p>
+            <CoverLetterPDF
+                name={jobApp.profile.name}
+                phone={jobApp.profile.telephone}
+                email={jobApp.profile.email}
+                address={jobApp.profile.location}
+                company={jobApp.job.company}
+                companyLocation={jobApp.job.location}
+                bodyText={jobApp.userCoverLetter}
             />
         </>
     );
