16a680c4f53a1893af593fe4e1aa810ff3eebda3
Will Bennett <41313772+willmbennett@users.noreply.github.com>
2024-01-31 12:01:12 -0800

    didn't like head

--- app/return/layout.tsx
+++ app/return/layout.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import Script from "next/script";
 
 export default async function AppLayout({
@@ -8,21 +7,17 @@ export default async function AppLayout({
 }) {
 
     return (
-        <>
-            <Head>
-                {/* Event snippet for conversion tracking */}
-                <Script
-                    id="gtag-conversion"
-                    strategy="afterInteractive"
-                    dangerouslySetInnerHTML={{
-                        __html: `
+        <div className="w-screen min-h-screen">
+            {/* Event snippet for conversion tracking */}
+            <Script
+                id="gtag-conversion"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
             gtag('event', 'conversion', {'send_to': 'AW-11370402046/IauhCJ6r040ZEP6h6q0q'});
           `,
-                    }}
-                />
-            </Head>
-            <div className="w-screen min-h-screen">
-                {children}
-            </div>
-        </>);
+                }}
+            />
+            {children}
+        </div>);
 }
