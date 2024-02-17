984a6fd6b80b51e4af4120ef4989af32e0dd75db
Jason Chen <jason.chen.23@dartmouth.edu>
2024-02-17 15:41:18 -0800

    fixed onboarding for mobile view

--- app/profile/[id]/layout.tsx
+++ app/profile/[id]/layout.tsx
@@ -19,15 +19,12 @@ export default async function AppLayout({
     return (
         <div className='min-h-screen'>
             <section className="flex flex w-full justify-center pt-5 md:pt-8 xl:pt-10">
-                {/* Hero section content */}
-                <Container className="flex flex-row justify-center w-full">
-                    {/* Hero text and buttons */}
-                    {children}
+                <Container className="flex flex-col md:flex-row justify-center w-full">
+                        {children}
                     <div className="w-full md:w-1/4">
                         <FeedbackAside />
                     </div>
                 </Container>
-
             </section>
         </div>
     )
