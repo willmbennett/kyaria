2281429b616dc308cdfdb2018b9bcc1a6f1d39f5
Jason Chen <jason.chen.23@dartmouth.edu>
2024-03-12 18:22:34 -0700

    use debounce from lodash

--- app/components/apps/pages/Notes.tsx
+++ app/components/apps/pages/Notes.tsx
@@ -3,6 +3,7 @@
 import React, {useState, useCallback} from 'react';
 import ReactQuill from 'react-quill';
 import { updateJobAppAction } from '../../../board/_action';
+import debounce from 'lodash/debounce';
 import 'react-quill/dist/quill.snow.css';
 import "./styles.css";
 
@@ -15,24 +16,14 @@ export default function Notes({ jobAppId, content }: NotesProps): JSX.Element {
   const [text, setText] = useState<string>(content || '');
   const [isSaved, setIsSaved] = useState<boolean>(true);
 
-  const debounce = (func: Function, delay: number) => {
-    let timeoutId: NodeJS.Timeout;
-    return (...args: any[]) => {
-      clearTimeout(timeoutId);
-      timeoutId = setTimeout(() => {
-        func.apply(null, args);
-      }, delay);
-    };
-  };
-
-  const debouncedHandleChange = debounce(async (value: string) => {
+  const debouncedHandleChange = useCallback(debounce(async (value: string) => {
     const setKey = 'notes';
     const data = JSON.parse(`{"${setKey}":""}`);
     data[setKey] = value;
 
     const update = updateJobAppAction(jobAppId, data, '/');
     setIsSaved(true);
-  }, 1000);
+  }, 1000), []);
 
   const handleChange = useCallback((value: string) => {
     setText(value);
