4fb0f6db6f3f36c9a1a4c5a3a11fd40480bdbc3a
will-kyaria <will@kyaria.ai>
2024-03-15 13:45:55 -0700

    removed themeprovider

--- app/components/chatbot/sidebar/Providers.tsx
+++ app/components/chatbot/sidebar/Providers.tsx
@@ -1,17 +1,14 @@
 'use client'
 
 import * as React from 'react'
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { ThemeProviderProps } from 'next-themes/dist/types'
 import { SidebarProvider } from '../../../../lib/chatbot/use-sidebar'
 import { TooltipProvider } from '../../ui/tooltip'
 
 export function Providers({ children, ...props }: ThemeProviderProps) {
     return (
-        <NextThemesProvider {...props}>
-            <SidebarProvider>
-                <TooltipProvider>{children}</TooltipProvider>
-            </SidebarProvider>
-        </NextThemesProvider>
+        <SidebarProvider>
+            <TooltipProvider>{children}</TooltipProvider>
+        </SidebarProvider>
     )
 }
