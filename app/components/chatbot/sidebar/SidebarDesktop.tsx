cee136b6ecf93a44a3d90f212d68e22d42ef7810
will-kyaria <will@kyaria.ai>
2024-03-15 13:58:28 -0700

    added ts ignore

--- app/components/chatbot/sidebar/SidebarDesktop.tsx
+++ app/components/chatbot/sidebar/SidebarDesktop.tsx
@@ -2,10 +2,11 @@ import { ChatHistory } from "./ChatHistory";
 import { Sidebar } from "./Sidebar";
 
 
-export async function SidebarDesktop({ userId }: { userId: string }) {
+export const SidebarDesktop = ({ userId }: { userId: string }) => {
 
     return (
         <Sidebar className="peer absolute inset-y-24 z-30 hidden -translate-x-full border-r bg-muted duration-300 ease-in-out data-[state=open]:translate-x-0 lg:flex lg:w-[250px] xl:w-[300px]">
+            {/* @ts-ignore */}
             <ChatHistory userId={userId} />
         </Sidebar>
     )
