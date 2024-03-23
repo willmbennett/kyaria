dacf8aecbeba8d1c3119676500b4a05766555ef8
Jason Chen <jason.chen.23@dartmouth.edu>
2024-03-22 19:43:04 -0700

    Fixed resume upload error

--- lib/hooks/use-create-resume.tsx
+++ lib/hooks/use-create-resume.tsx
@@ -35,10 +35,9 @@ const useSubmitResume = ({ onError, onSuccess }: UseSubmitResumeProps) => {
                 if (!resumeId) {
                     throw new Error("Failed to create resume");
                 }
-              //console.log('resumeId: ', resumeId);
 
                 if (onSuccess) {
-                    await onSuccess('someResumeId'); // Awaiting onSuccess callback
+                    await onSuccess(resumeId); // Awaiting onSuccess callback
                 }
             }
         } catch (error) {
