a6f17b6ae8de00c58c98419204345d7b5c3f2f66
will-kyaria <will@kyaria.ai>
2024-06-05 10:04:51 -0700

    Added recording indicator

--- app/components/chatbot/VideoChatContainer.tsx
+++ app/components/chatbot/VideoChatContainer.tsx
@@ -69,10 +69,23 @@ export const VideoChatContainer = ({ userId, chatId, threadId, messages, activeS
 
     const handleVideoComponent = activeSubscription ? renderVideoChatComponent : handleSubscription
 
+    const recordingPing = <div className='flex gap-2 items-center'>
+        <span className="relative flex h-3 w-3">
+            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-500 opacity-75"></span>
+            <span className="relative inline-flex rounded-full h-3 w-3 bg-red-500"></span>
+        </span>
+        <p className='text-red-500'>Recording</p>
+    </div>
+
     return (
         <div className={cn(`flex flex-col md:flex-row h-full  w-full justify-center text-center gap-4 sm:p-1 md:p-2 lg:p-3 xl:p-4 overflow-hidden`, interviewing && 'bg-slate-100')}>
             <div className='flex flex-col w-full justify-end h-full gap-2'>
-                {(interviewName && mockInterviewId) && <Link className="text-left font-bold text-lg font-slate-800" href={`/mockinterviews/${mockInterviewId}`}>{interviewName}</Link>}
+                {interviewName && mockInterviewId && (
+                    <div className='flex gap-4'>
+                        <Link className="text-left font-bold text-lg font-slate-800" href={`/mockinterviews/${mockInterviewId}`}>{interviewName}</Link>
+                        {recordingPing}
+                    </div>
+                )}
                 {handleVideoComponent}
             </div>
             <Chat messages={chatMessages} showTranscript={showTranscript} />
