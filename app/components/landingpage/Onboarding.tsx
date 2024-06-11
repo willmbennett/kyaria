a74e5cb88ca8da86ade72be5f3d519728d1f34f0
will-kyaria <will@kyaria.ai>
2024-06-11 12:04:28 -0700

    added organization form

--- app/components/landingpage/Onboarding.tsx
+++ app/components/landingpage/Onboarding.tsx
@@ -13,12 +13,13 @@ export default function HomeHero() {
     <section className="relative overflow-hidden pt-16 md:pt-20 xl:pt-32">
       <Container className='flex flex-col items-center justify-center gap-10'>
         <h1 className="text-5xl font-semibold leading-tighter text-slate-900 md:mx-auto md:max-w-2xl text-center xl:mx-0 xl:text-6xl">
-          Let's create your first application packet
+          Let's add your first job application
         </h1>
-        <div className="flex flex-col max-w-3xl gap-4">
+        <div className="flex flex-col max-w-3xl gap-4 text-center">
           <h2 className="text-xl font-semibold  text-center leading-tighter text-slate-900 md:mx-auto md:max-w-2xl md:text-center xl:mx-0 xl:text-3xl">
-            Step 1. Drop your resume
+            Step 1. Add your resume
           </h2>
+          <p className='text-sm italic'>We'll use this resume to conduct realistic mock interviews, craft compelling elevator pitches, help you talk about your accomplishments, and more. </p>
           {resume ?
             <div className='flex gap-2 items-center justify-center'>
               <p>Resume created!</p>
@@ -28,10 +29,11 @@ export default function HomeHero() {
           }
         </div>
         {resume &&
-          <div className="flex flex-col max-w-3xl gap-4">
+          <div className="flex flex-col max-w-3xl gap-4 text-center">
             <h2 className="text-xl font-semibold  text-center leading-tighter text-slate-900 md:mx-auto md:max-w-2xl md:text-center xl:mx-0 xl:text-3xl">
               Step 2. Add a link to a job post
             </h2>
+            <p className='text-sm italic'>We'll go ahead and fetch the post for you.</p>
             {application ?
               <div className='flex gap-2 items-center justify-center'>
                 <p>Application packet created!</p>
---
+++ app/organizations/page.jsx
@@ -0,0 +1,15 @@
+import { OrganizationsHeader } from '../components/landingpage/OrganizationsHeader'
+
+export const metadata = {
+  title: 'Kyar.ai - Organizations',
+  description:
+    'Are you a bootcamp or university looking for career services software? Kyaria.ai offers robust solutions for organizations to empower their students.',
+}
+
+export default function OrganizationsPage() {
+  return (
+    <>
+      <OrganizationsHeader />
+    </>
+  )
+}
